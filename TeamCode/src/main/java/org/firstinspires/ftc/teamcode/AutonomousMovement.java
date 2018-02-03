d093de2164bf877453638bdc0aacae4cba403a5e
PramodhArya <pramodharya@gmail.com>
2018-02-03 11:12:09 -0500

    fixed init problem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMovement.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class AutonomousMovement {
+public class AutonomousMovement{
 
-    private DcMotor motorLeft, motorRight, motorMiddle, motorLift;
-    private Servo leftGrabber, rightGrabber, colorKnocker;
+    private DcMotor motor0, motor1, motor2, motor3;
+    private Servo leftGrabber, rightGrabber, servo3;
     private AutoTimer aTimer;
     private final int MOTOR_0_DIRECTION = 1;
     private final int MOTOR_1_DIRECTION = -1;
@@ -17,103 +17,22 @@ public class AutonomousMovement {
     private double powerScale = (1/1.2);
     private int gear = 2;
     public boolean isPressed = false;
-    double revAngle = 0;
-    double currAngle = revAngle;
 
     public AutonomousMovement(DcMotor motor0, DcMotor motor1, DcMotor motor2, DcMotor motor3, Servo servo1, Servo servo2, Servo servo3) {
 
-        this.motorLeft = motor0;
-        this.motorRight = motor1;
-        this.motorMiddle = motor3;
-        this.motorLift = motor2;
+        this.motor0 = motor0;
+        this.motor1 = motor1;
+        this.motor2 = motor3;
+        this.motor3 = motor2;
         this.leftGrabber = servo1;
         this.rightGrabber = servo2;
-        this.colorKnocker = servo3;
+        this.servo3 = servo3;
 
-        motorLeft.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
-        motorRight.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        this.motor0.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        this.motor1.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
 
     }
 
-    public void move(String direction, long time) {
-        aTimer = new AutoTimer(time);
-        while (!aTimer.checkTime()) {
-            if (direction.equals("back")) {
-                motorRight.setPower(-.5);
-                motorLeft.setPower(.4);
-            } else if (direction.equals("fwd")) {
-                motorRight.setPower(.5);
-                motorLeft.setPower(-.4);
-            } else if (direction.equals("right")) {
-                motorMiddle.setPower(-1);
-            } else if (direction.equals("left")) {
-                motorMiddle.setPower(1);
-            } else {
-                motorRight.setPower(0);
-                motorLeft.setPower(0);
-                motorMiddle.setPower(0);
-            }
-        }
-        motorRight.setPower(0);
-        motorLeft.setPower(0);
-        motorMiddle.setPower(0);
-        pause();
-    }
 
-    public void turn(String direction, long time) {
-        aTimer = new AutoTimer(time);
-        while (!aTimer.checkTime()) {
-            if (direction.equals("left")) {
-                motorLeft.setPower(-.4);
-                motorRight.setPower(-.5);
-            } else if (direction.equals("right")) {
-                motorRight.setPower(.5);
-                motorLeft.setPower(.4);
-            } else {
-                motorRight.setPower(0);
-                motorLeft.setPower(0);
-            }
-        }
-        motorRight.setPower(0);
-        motorLeft.setPower(0);
-        pause();
-    }
-
-    public void lift(String direction, long time) {
-        aTimer = new AutoTimer(time);
-        while (!aTimer.checkTime()) {
-            if (direction.equals("up")) {
-                motorLift.setPower(.5);
-            } else if (direction.equals("down")) {
-                motorLift.setPower(-.5);
-            } else {
-                motorLift.setPower(0);
-            }
-        }
-        motorLift.setPower(0);
-        pause();
-    }
-
-    public void dropdown(String direction, long time) {
-        aTimer = new AutoTimer(time);
-        while (!aTimer.checkTime()) {
-            if (direction.equals("right")) {
-                colorKnocker.setPosition(-1);
-            } else if (direction.equals("left")) {
-                colorKnocker.setPosition(1);
-            } else {
-                colorKnocker.setPosition(.5);
-            }
-        }
-        colorKnocker.setPosition(.5);
-        pause();
-    }
-
-    private void pause() {
-        aTimer = new AutoTimer(750);
-        while(!aTimer.checkTime()) {
-
-        }
-    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Code.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,8 +9,33 @@ import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Autonomous
 public class Autonomous_Code extends LinearOpMode{
+
+    BNO055IMU imu;
+
+    public Orientation angles;
+    public Acceleration gravity;
+
+    public double angle;
+
+    private DcMotor motorLeft, motorRight, motorMiddle;
+    private int motorLeftDirection = 1;
+    private int motorRightDirection = -1;
+
+    public Timer timer;
+    public TimerTask task;
 
     /** INITS **/
     private DcMotor motor0;
@@ -24,9 +51,6 @@ public class Autonomous_Code extends LinearOpMode{
     private Grabber grabber;
     private VerticalLiftMotor liftMotor;
     private VuMarkIdentification vuMarkIdentification;
-    private AutonomousMovement moveAuto;
-    private AutoTimer aTimer;
-    public DriveTrain turnAngle;
 //    private Timer timer;
 //    private TimerTask task;
 
@@ -67,8 +91,8 @@ public class Autonomous_Code extends LinearOpMode{
         //HARDWARE MAPS
         motor0 = hardwareMap.get(DcMotor.class, "motor0");
         motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motor3 = hardwareMap.get(DcMotor.class, "motor2");
+        motor2 = hardwareMap.get(DcMotor.class, "motor3");
         servo0 = hardwareMap.get(Servo.class, "servo0");
         servo1 = hardwareMap.get(Servo.class, "servo1");
         servo2 = hardwareMap.get(Servo.class, "servo2");
@@ -80,11 +104,24 @@ public class Autonomous_Code extends LinearOpMode{
         grabber = new Grabber(servo1, servo2);
         liftMotor = new VerticalLiftMotor(motor2);
         vuMarkIdentification = new VuMarkIdentification(hardwareMap, telemetry);
-        turnAngle = new DriveTrain(motor0, motor1, motor3, hardwareMap);
-        moveAuto = new AutonomousMovement(motor0, motor1, motor2, motor3, servo1, servo2, servo3);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
         motor0.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
         motor1.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
-        turnAngle.startUpdates();
+
+        startUpdates();
     }
 
     /** SEQUENCES **/
@@ -105,14 +142,14 @@ public class Autonomous_Code extends LinearOpMode{
 
     public void end() {
 //        columnShift();
-        moveAuto.move("fwd", 450);
+        move("fwd", 450);
         ungrab();
-        moveAuto.move("back", 450);
+        move("back", 450);
         grab();
         liftDown();
-        moveAuto.move("fwd", 500);
-        moveAuto.move("back", 150);
-        turnAngle.stopUpdates();
+        move("fwd", 500);
+        move("back", 150);
+        stopUpdates();
     }
 
     public void align(String direction) {
@@ -120,15 +157,15 @@ public class Autonomous_Code extends LinearOpMode{
         long alignT = 1500;
         long backout = 450;
         if(direction.equals("red")) {
-            moveAuto.move("back", first);
+            move("back", first);
             pause(1000);
-            turnAngle.turnAbsolute(0);
+            turnAbsolute(0);
 //            moveAuto.move("fwd", alignT);
 //            moveAuto.move("back", backout);
         } else {
-            moveAuto.move("front", first);
+            move("front", first);
             pause(1000);
-            turnAngle.turnAbsolute(0);
+            turnAbsolute(0);
 //            moveAuto.move("fwd", alignT);
 //            moveAuto.move("back", backout);
         }
@@ -136,47 +173,46 @@ public class Autonomous_Code extends LinearOpMode{
 
     /** CASES **/
     public void redTop() {
-        moveAuto.move("back", 500);
-        turnAngle.turnAbsolute(90);
-        moveAuto.move("fwd", 450);
+        move("back", 1000);
+        turnAbsolute(90);
         end();
     }
 
     public void blueBottom() {
-        moveAuto.move("left", 1000);
-        turnAngle.turnAbsolute(0);
+        move("left", 1000);
+        turnAbsolute(0);
         pause(1000);
-        moveAuto.move("fwd", 250);
-        moveAuto.move("left", columnTime);
+        move("fwd", 250);
+        move("left", columnTime);
 //        if (column == 1) {
 //            moveAuto.move("right", columnTime + 100);
 //        } else if (column == 2) {
 //            moveAuto.move("right", columnTime + 200);
 //        }
-        turnAngle.turnAbsolute(0);
+        turnAbsolute(0);
         end();
     }
 
     public void blueTop() {
-        moveAuto.move("fwd", 1000);
-        turnAngle.turnAbsolute(-90);
+        move("fwd", 1000);
+        turnAbsolute(90);
         end();
     }
 
     public void redBottom() {
-        moveAuto.move("right", 1000);
+        move("right", 1000);
 //        moveAuto.turn("right", 1850);
 //        pause(100);
-        turnAngle.turnAbsolute(180);
+        turnAbsolute(180);
         pause(1000);
-        moveAuto.move("fwd", 250);
-        moveAuto.move("right", columnTime);
+        move("fwd", 250);
+        move("right", columnTime);
 //        if (column == 1) {
 //            moveAuto.move("right", columnTime + 100);
 //        } else if (column == 2) {
 //            moveAuto.move("right", columnTime + 200);
 //        }
-        turnAngle.turnAbsolute(180);
+        turnAbsolute(180);
         end();
     }
 
@@ -192,12 +228,12 @@ public class Autonomous_Code extends LinearOpMode{
     }
 
     private void knockRight() {
-        moveAuto.dropdown("right", knockTime);
+        dropdown("right", knockTime);
 
     }
 
     private void knockLeft() {
-        moveAuto.dropdown("left", knockTime);
+        dropdown("left", knockTime);
 
 
     }
@@ -258,19 +294,20 @@ public class Autonomous_Code extends LinearOpMode{
     }
 
     public void liftUp() {
-        moveAuto.lift("up", 800);
+        lift("up", 800);
         pause(1000);
     }
 
     public void liftDown() {
-        moveAuto.lift("down", 750);
+        lift("down", 750);
         pause(450);
     }
 
     /** COLUMN **/
     public void pickColumn() {
-        aTimer = new AutoTimer(3000);
-        while (column == 3 && !aTimer.checkTime()) {
+        long duration = 3000;
+        long endTime = System.currentTimeMillis() + duration;
+        while (column == 3 && !checkTime(endTime) && opModeIsActive()) {
             column= vuMarkIdentification.identify();
             telemetry.addData("column: ", column);
             telemetry.update();
@@ -292,26 +329,139 @@ public class Autonomous_Code extends LinearOpMode{
 
     /** TIMER **/ //NEEDS MODDING
     private void pause(long time) {
-        aTimer = new AutoTimer(time);
-        while(!aTimer.checkTime()) {
+
+        long endTime = System.currentTimeMillis() + time;
+        while(!checkTime(endTime) && opModeIsActive()) {
 
         }
     }
 
-//    public void timer(final double time) {
-//        timePassed = 0;
-//        period = 1000;
-//
-//        timer = new Timer();
-//        task = new TimerTask() {
-//            @Override
-//            public void run() {
-//                timePassed += period;
-//                if ((timePassed / period) <= time) {
-//                    timer.cancel();
-//                }
-//            }
-//        };
-//    }
+    /** MOVEMENT **/
+
+    public void move(String direction, long time) {
+        long endTime = System.currentTimeMillis() + time;
+        while (!checkTime(endTime) && opModeIsActive()) {
+            if (direction.equals("back")) {
+                motor1.setPower(-.5);
+                motor0.setPower(.4);
+            } else if (direction.equals("fwd")) {
+                motor1.setPower(.5);
+                motor0.setPower(-.4);
+            } else if (direction.equals("right")) {
+                motor2.setPower(-1);
+            } else if (direction.equals("left")) {
+                motor2.setPower(1);
+            } else {
+                motor1.setPower(0);
+                motor0.setPower(0);
+                motor2.setPower(0);
+            }
+        }
+        motor1.setPower(0);
+        motor0.setPower(0);
+        motor2.setPower(0);
+        pause();
+    }
+
+    public void turn(String direction, long time) {
+        long endTime = System.currentTimeMillis() + time;
+        while (!checkTime(endTime) && opModeIsActive()) {
+            if (direction.equals("left")) {
+                motor0.setPower(-.4);
+                motor1.setPower(-.5);
+            } else if (direction.equals("right")) {
+                motor1.setPower(.5);
+                motor0.setPower(.4);
+            } else {
+                motor1.setPower(0);
+                motor0.setPower(0);
+            }
+        }
+        motor1.setPower(0);
+        motor0.setPower(0);
+        pause();
+    }
+
+    public void lift(String direction, long time) {
+        long endTime = System.currentTimeMillis() + time;
+        while (!checkTime(endTime) && opModeIsActive()) {
+            if (direction.equals("up")) {
+                motor3.setPower(.5);
+            } else if (direction.equals("down")) {
+                motor3.setPower(-.5);
+            } else {
+                motor3.setPower(0);
+            }
+        }
+        motor3.setPower(0);
+        pause();
+    }
+
+    public void dropdown(String direction, long time) {
+        long endTime = System.currentTimeMillis() + time;
+        while (!checkTime(endTime) && opModeIsActive()) {
+            if (direction.equals("right")) {
+                servo3.setPosition(-1);
+            } else if (direction.equals("left")) {
+                servo3.setPosition(1);
+            } else {
+                servo3.setPosition(.5);
+            }
+        }
+        servo3.setPosition(.5);
+        pause();
+    }
+
+    private void pause() {
+        long endTime = System.currentTimeMillis() + 750;
+        while(!checkTime(endTime) && opModeIsActive()) {
+
+        }
+    }
+
+    public void turnAbsolute(float angle) {
+
+        float marginOfError = 12;
+
+        float right = angle - marginOfError;
+        float left = angle + marginOfError;
+
+        while (angles == null && opModeIsActive()) {
+
+        }
+
+        float multiplier = angle / Math.abs(angle);
+
+        while (!(angles.firstAngle < left && angles.firstAngle > right) && opModeIsActive()) {
+            motor0.setPower(-0.35f * multiplier);
+            motor1.setPower(-0.35f * multiplier);
+        }
+
+        motor0.setPower(0);
+        motor1.setPower(0);
+    }
+
+    public void startUpdates() {
+        timer = new Timer();
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            }
+        };
+        timer.scheduleAtFixedRate(task, 0, 1);
+    }
+
+    public void stopUpdates() {
+        timer.cancel();
+    }
+
+    public boolean checkTime(long endTime) {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime > endTime) {
+            return true;
+        }
+        return false;
+    }
 
 }
