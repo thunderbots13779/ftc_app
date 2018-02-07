c4bc293287580bfb0c3cafc2ef49b0ce7091ff7e
lappogutidev <lappoguti00@gmail.com>
2018-02-07 14:11:56 -0500

    Added end action

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/End.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+public class End implements Action {
+
+    public void initialize() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public boolean check() {
+        return true;
+    }
+
+    public void end() {
+
+    }
+
+}
