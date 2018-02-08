47086ba6279076728eb4e6422ee0fa672b996efb
PramodhArya <pramodharya@gmail.com>
2018-02-07 20:27:18 -0500

    flywheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -13,12 +13,16 @@ public class Test extends LinearOpMode {
 
     private Servo servo0;
     private Servo servo1;
+    private DcMotor motor0;
+    private DcMotor motor1;
 
     @Override
     public void runOpMode() {
 
-        servo0 = hardwareMap.get(Servo.class, "servo0");
-        servo1 = hardwareMap.get(Servo.class, "servo1");
+//        servo0 = hardwareMap.get(Servo.class, "servo0");
+//        servo1 = hardwareMap.get(Servo.class, "servo1");
+          motor0 = hardwareMap.get(DcMotor.class, "motor0");
+          motor1 = hardwareMap.get(DcMotor.class, "motor1");
 
         //Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -27,20 +31,36 @@ public class Test extends LinearOpMode {
         while (opModeIsActive()) {
 
             if (this.gamepad1.left_stick_x > 0) {
-                servo0.setPosition(1);
+                motor0.setPower(1);
             } else if (this.gamepad1.left_stick_x < 0) {
-                servo0.setPosition(0);
+                motor0.setPower(-1);
             } else {
-                servo0.setPosition(1);
+                motor0.setPower(0);
             }
 
             if (this.gamepad1.right_stick_x > 0) {
-                servo1.setPosition(1);
+                motor1.setPower(1);
             } else if (this.gamepad1.right_stick_x < 0) {
-                servo1.setPosition(0);
+                motor1.setPower(-1);
             } else {
-                servo1.setPosition(1);
+                motor1.setPower(0);
             }
+
+//            if (this.gamepad1.left_stick_x > 0) {
+//                servo0.setPosition(1);
+//            } else if (this.gamepad1.left_stick_x < 0) {
+//                servo0.setPosition(0);
+//            } else {
+//                servo0.setPosition(1);
+//            }
+//
+//            if (this.gamepad1.right_stick_x > 0) {
+//                servo1.setPosition(1);
+//            } else if (this.gamepad1.right_stick_x < 0) {
+//                servo1.setPosition(0);
+//            } else {
+//                servo1.setPosition(1);
+//            }
         }
 
     }
