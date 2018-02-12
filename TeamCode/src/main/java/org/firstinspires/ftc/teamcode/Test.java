50be7eb1583206a10e358a232f2857f5fe8eaa13
PramodhArya <pramodharya@gmail.com>
2018-02-12 16:57:17 -0500

    dropper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -11,18 +11,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class Test extends LinearOpMode {
 
-    private Servo servo0;
-    private Servo servo1;
     private DcMotor motor0;
-    private DcMotor motor1;
 
     @Override
     public void runOpMode() {
 
-//        servo0 = hardwareMap.get(Servo.class, "servo0");
-//        servo1 = hardwareMap.get(Servo.class, "servo1");
           motor0 = hardwareMap.get(DcMotor.class, "motor0");
-          motor1 = hardwareMap.get(DcMotor.class, "motor1");
 
         //Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -30,37 +24,15 @@ public class Test extends LinearOpMode {
         //run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            if (this.gamepad1.left_stick_x > 0) {
+            if(gamepad1.left_bumper) {
                 motor0.setPower(1);
-            } else if (this.gamepad1.left_stick_x < 0) {
+            } else if (gamepad1.right_bumper) {
                 motor0.setPower(-1);
             } else {
                 motor0.setPower(0);
             }
 
-            if (this.gamepad1.right_stick_x > 0) {
-                motor1.setPower(1);
-            } else if (this.gamepad1.right_stick_x < 0) {
-                motor1.setPower(-1);
-            } else {
-                motor1.setPower(0);
-            }
 
-//            if (this.gamepad1.left_stick_x > 0) {
-//                servo0.setPosition(1);
-//            } else if (this.gamepad1.left_stick_x < 0) {
-//                servo0.setPosition(0);
-//            } else {
-//                servo0.setPosition(1);
-//            }
-//
-//            if (this.gamepad1.right_stick_x > 0) {
-//                servo1.setPosition(1);
-//            } else if (this.gamepad1.right_stick_x < 0) {
-//                servo1.setPosition(0);
-//            } else {
-//                servo1.setPosition(1);
-//            }
         }
 
     }
